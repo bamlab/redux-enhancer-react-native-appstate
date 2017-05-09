e53b246f732cf98932699c0170873a45cd95c01c
alphasp <gmerudotcom@gmail.com>
2017-05-09 14:51:22 +0800

    added BACKGROUND and INACTIVE state

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { AppState } from 'react-native';
 
 export const FOREGROUND = 'APP_STATE.FOREGROUND';
+export const BACKGROUND = 'APP_STATE.BACKGROUND';
+export const INACTIVE = 'APP_STATE.INACTIVE';
 
 export default () => (createStore) => (...args) => {
   const store = createStore(...args);
@@ -8,10 +10,20 @@ export default () => (createStore) => (...args) => {
   let currentState = AppState.currentState;
 
   const handleAppStateChange = (nextAppState) => {
-    if (currentState.match(/inactive|background/) && nextAppState === 'active') {
-      store.dispatch({
-        type: FOREGROUND,
-      });
+    if (currentState !== nextAppState) {
+      let type;
+      if (nextAppState === 'active') {
+        type = FOREGROUND;
+      } else if (nextAppState === 'background') {
+        type = BACKGROUND;
+      } else if (nextAppState === 'inactive') {
+        type = INACTIVE;
+      }
+      if (type) {
+        store.dispatch({
+          type,
+        });
+      }
     }
     currentState = nextAppState;
   };
